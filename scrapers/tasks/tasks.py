d4b8ed3f80441cd86e9830e3ddc6f5716a10f5a2
Joel Clayton <grahamjordan2596@gmail.com>
2026-03-18 09:33:17 -0700

    reorg

---
+++ scrapers/tasks/tasks.py
@@ -0,0 +1,9 @@
+from celery import Celery
+
+app = Celery('tasks')
+app.conf.broker_url = 'redis://localhost:6379/0'
+app.conf.result_backend = 'redis://localhost:6379/1'
+
+@app.task
+def add(x, y):
+    return x + y
