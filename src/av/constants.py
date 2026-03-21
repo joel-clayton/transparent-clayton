07fcaa34a7ade901a6e74896e033a6b6bee9fb29
Gautam Joshi <g@gautamjoshi.com>
2026-03-21 00:59:57 -0700

    add youtube uploader with partial metadata that needs to be resolved

---
+++ src/av/constants.py
@@ -0,0 +1,16 @@
+SNIPPET = dict(
+    defaultAudioLanguage="English",
+)
+
+STATUS = dict(
+    publicStatsViewable=True,
+    selfDeclaredMadeForKids=False,
+    containsSyntheticMedia=False,
+)
+
+CONTENT_DETAILS = dict(
+    licensedContent=False,
+    caption="False",
+)
+
+CC_MTG_REDIS_KEY = "cc_mtg"
